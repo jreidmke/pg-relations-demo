a71fdcddfadb092bb5bc257c7c012c6183316fc3
unknown <jreidmke@gmail.com>
2020-10-22 13:30:03 -0500

    new routing for msging and users

--- VideoCode/routes/users.js
+++ VideoCode/routes/users.js
@@ -7,36 +7,45 @@ const ExpressError = require("../expressError");
 
 
 
-/** Get users: [user, user, user] */
-router.get("/", async function (req, res, next) {
-  try {
-    const results = await db.query(
-      `SELECT id, name, type FROM users`);
+// /** Get users: [user, user, user] */
+// router.get("/", async function (req, res, next) {
+//   try {
+//     const results = await db.query(
+//       `SELECT id, name, type FROM users`);
 
-    return res.json(results.rows);
-  }
+//     return res.json(results.rows);
+//   }
 
-  catch (err) {
-    return next(err);
-  }
-});
+//   catch (err) {
+//     return next(err);
+//   }
+// });
 
-router.get('/:id', async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const userResults = await db.query(`SELECT name, type FROM users WHERE id = $1`, [id]);
-    const msgResults = await db.query(`SELECT id, msg FROM messages WHERE user_id = $1`, [id]);
-    if (userResults.rows.length === 0) {
-      throw new ExpressError("User not found", 404);
-    }
-    const user = userResults.rows[0];
-    user.messages = msgResults.rows;
-    return res.send(user)
-  } catch (e) {
-    return next(e)
-  }
-})
+// router.get('/:id', async (req, res, next) => {
+//   const { id } = req.params;
+//   try {
+//     const userResults = await db.query(`SELECT name, type FROM users WHERE id = $1`, [id]);
+//     const msgResults = await db.query(`SELECT id, msg FROM messages WHERE user_id = $1`, [id]);
+//     if (userResults.rows.length === 0) {
+//       throw new ExpressError("User not found", 404);
+//     }
+//     const user = userResults.rows[0];
+//     user.messages = msgResults.rows;
+//     return res.send(user)
+//   } catch (e) {
+//     return next(e)
+//   }
+// })
 
 /** Get user: {name, type, messages: [{msg, msg}]} */
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const results = await db.query(`SELECT u.name, u.type, m.id, m.msg FROM users AS u JOIN messages AS m ON u.id = m.user_id WHERE u.id=$1`, [req.params.id]);
+    return res.json(results.rows);
+  } catch (error) {
+    next(error);
+  }
+})
 
 module.exports = router;
